10deb0d0ab11c33f609972755c3d45c6f2247570
evgenFrontendTop6 <saharovnewlife@gmail.com>
2021-08-10 11:20:07 +0400

    general code

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+        analytics: './src/analytics.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
