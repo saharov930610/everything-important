0365e0114da5b081df402ced823779fd11dbceef
evgenFrontendTop6 <saharovnewlife@gmail.com>
2021-08-11 21:45:04 +0400

    file loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Post from "./Post";
+import json from './assets/json.json';
+import WebpackLogo from "./assets/webpack-logo.png";
 import "./styles/styles.css";
 
-const post = new Post("Webpack Post Title")
+const post = new Post("Webpack Post Title", WebpackLogo)
 
 console.log("Post to String:", post.toString())
+
+console.log("JSON:", json);
--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             }
         ]
     }
