77f00a76b9af75c7b49d91c5ab4e2ff99875351e
evgenFrontendTop6 <saharovnewlife@gmail.com>
2021-08-16 15:45:31 +0400

    dynamic lodash

--- src/babel.js
+++ src/babel.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 async function start () {
     return await Promise.resolve('async is working')
 }
@@ -11,3 +13,8 @@ class Util {
 }
 
 console.log("Util id:", Util.id);
+
+
+import('lodash').then(({ default: _ }) => {
+    console.log('Lodash 1111', _.random(0, 42, true))
+})
